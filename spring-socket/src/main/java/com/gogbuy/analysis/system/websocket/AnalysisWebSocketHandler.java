078cc532be4c60511f764aa89ce77b93ac2d37f9
朱昊 <hao.zhu@ffzxnet.com>
2018-02-01 14:50:05 +0800

    spring websocket

---
+++ spring-socket/src/main/java/com/gogbuy/analysis/system/websocket/AnalysisWebSocketHandler.java
@@ -0,0 +1,57 @@
+package com.gogbuy.analysis.system.websocket;
+
+import org.apache.commons.collections.MapUtils;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+/**
+ * Created by Mr.Yangxiufeng on 2016/5/27.
+ * 09
+ * 23
+ * SpringWebApp
+ */
+public class AnalysisWebSocketHandler extends TextWebSocketHandler {
+
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+        String userId = MapUtils.getString(session.getAttributes(), "userId");
+        //记录连接的session
+        WebSocketSessionUtils.add(userId, session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+        System.out.println("handleTextMessage");
+        String userId = MapUtils.getString(session.getAttributes(), "userId");
+        String partnerInfo = MapUtils.getString(session.getAttributes(), "partnerInfo");
+        if (null != partnerInfo) {
+            //TODO 进行配对
+        } else {
+            //TODO 处理其他消息
+            TextMessage returnMessage = new TextMessage("Hi:" + userId + "..your message:" + message.getPayload() + " I have received,now I tell you");
+            WebSocketSessionUtils.sendMessageToTarget(userId, returnMessage);
+        }
+
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        super.handleTransportError(session, exception);
+        System.out.println("handleTransportError");
+        String userId = MapUtils.getString(session.getAttributes(), "userId");
+        WebSocketSessionUtils.remove(userId);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+        System.out.println("afterConnectionClosed");
+        String userId = MapUtils.getString(session.getAttributes(), "userId");
+        WebSocketSessionUtils.remove(userId);
+    }
+}
---
+++ spring-socket/src/main/java/com/gogbuy/analysis/system/websocket/HandshakeInterceptor.java
@@ -0,0 +1,41 @@
+package com.gogbuy.analysis.system.websocket;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
+
+/**
+ * Created by Mr.Yangxiufeng on 2016/5/27.
+ * Time:9:57
+ * ProjectName:SpringWebApp
+ */
+public class HandshakeInterceptor extends HttpSessionHandshakeInterceptor {
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        if (getSession(request) != null) {
+            ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+            HttpServletRequest httpServletRequest = servletRequest.getServletRequest();
+            attributes.put("userId", httpServletRequest.getParameter("userId"));
+        }
+        return super.beforeHandshake(request, response, wsHandler, attributes);
+    }
+
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception ex) {
+        super.afterHandshake(request, response, wsHandler, ex);
+    }
+
+    private HttpSession getSession(ServerHttpRequest request) {
+        if (request instanceof ServletServerHttpRequest) {
+            ServletServerHttpRequest serverRequest = (ServletServerHttpRequest) request;
+            return serverRequest.getServletRequest().getSession(false);
+        }
+        return null;
+    }
+}
