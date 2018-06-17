c0750fb42f06d8529e5c9fafa9ef2013350dfe51
朱昊 <pighao@163.com>
2018-06-17 11:26:42 +0800

    ff

--- selenium-spider/src/main/java/com/zhu/selenium/selenium.java
+++ selenium-spider/src/main/java/com/zhu/selenium/selenium.java
@@ -11,8 +11,9 @@ public class selenium {
 
     public static void main(String[] args){
         System.setProperty("phantomjs.binary.path", "/Users/zhuhao/Downloads/phantomjs-2.1-2.1-macosx/bin/phantomjs");
-        WebDriver webDriver = new PhantomJSDriver();
-        //WebDriver webDriver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver","c:/chromedriver2.38.exe");//chromedriver服务地址
+       // WebDriver webDriver = new PhantomJSDriver();
+        WebDriver webDriver = new ChromeDriver();
         webDriver.get("http://daikuan.51kanong.com/Home/Daikuan/lists.html");
         WebElement webElement = webDriver.findElement(By.xpath("/html"));
         System.out.println(webElement.getAttribute("outerHTML"));
