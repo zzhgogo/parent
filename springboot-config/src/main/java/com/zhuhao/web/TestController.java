9d0f61df9c5efe3109e413ae1da876a89e706417
zhuhao <1536191986@qq.com>
2018-09-18 10:33:20 +0800

    增加邮件服务

--- springboot-config/src/main/java/com/zhuhao/web/TestController.java
+++ springboot-config/src/main/java/com/zhuhao/web/TestController.java
@@ -1,26 +1,20 @@
-package com.zhuhao.session;
+package com.zhuhao.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-import org.springframework.web.util.WebUtils;
 import redis.clients.jedis.JedisShardInfo;
 
-import javax.servlet.http.HttpServletRequest;
-
 @RestController
-public class AppController extends BaseController{
+public class TestController extends BaseController {
 
     @Autowired
     private RedisTemplate redisTemplate;
 
 
     @RequestMapping("/test")
-    public Object test(){
+    public Object test1(){
         JedisShardInfo jedisShardInfo;
         redisTemplate.opsForValue().set("name", "朱昊");
         return redisTemplate.opsForValue().get("name");
