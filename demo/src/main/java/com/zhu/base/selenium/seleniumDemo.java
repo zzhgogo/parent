163f4a9840c718f63b8048ae5d6315a4ee152c61
zhuhao <1536191986@qq.com>
2019-02-20 18:33:25 +0800

    selenium

--- demo/src/main/java/com/zhu/base/selenium/seleniumDemo.java
+++ demo/src/main/java/com/zhu/base/selenium/seleniumDemo.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 
 public class seleniumDemo {
@@ -19,14 +20,15 @@ public class seleniumDemo {
         //WebDriver webDriver = new PhantomJSDriver();
         WebDriver webDriver = new ChromeDriver();
         //WebDriver webDriver = new FirefoxDriver();
-        webDriver.get("http://toutiao.manqian.cn/");
+        webDriver.get("https://weixin.sogou.com/");
 
-        List<WebElement> webElements = webDriver.findElements(By.xpath("/html/body/div[4]/main/div/ul/li/div/h3/a"));
+        List<WebElement> webElements = webDriver.findElements(By.xpath("//*[@id=\"pc_0_0\"]/li/div[2]/h3/a"));
 
         String hander = webDriver.getWindowHandle();
 
-        webElements.stream().limit(2).forEach(webElement -> {
+        webElements.stream().forEach(webElement -> {
             webElement.click();
+            sleepRandom(1000, 2000);
             switchToOtherWindow(webDriver, hander);
             System.out.println(webDriver.getTitle());
             closeOtherWindowByHandler(webDriver, hander);
@@ -52,6 +54,7 @@ public class seleniumDemo {
 
         }
     }
+
     public static void switchToOtherWindow(WebDriver driver, String currentHandle) {
         try {
             if (StringUtils.isBlank(currentHandle)) {
@@ -72,7 +75,18 @@ public class seleniumDemo {
 
     }
 
-
+    /**
+     * 随机时间睡眠 多少毫秒到多少毫秒之间
+     */
+    public static void sleepRandom(int start, int end) {
+        Random random = new Random();
+        int mims = (int) Math.round(Math.random() * (end - start) + start);
+        try {
+            Thread.sleep(mims);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
     // https://sites.google.com/a/chromium.org/chromedriver/downloads
 //    public static void main(String[] args){
 //        System.setProperty("webdriver.firefox.bin", "/Users/zhuhao/firefox-sdk/bin/Firefox");
